cd1f5f1b27a6e5c86b29819636be0669e64fffb9
Matthew Rease <scubaventure101@hotmail.com>
2023-08-25 19:18:32 -0700

    Refactor (pylint)

--- build.py
+++ build.py
@@ -1,79 +1,85 @@
 #!/bin/python3
+"""Constructs a full NeoVim configuration directory, using a config file and
+other data files."""
 
 import json
 import os
+import sys
 
-config = json.loads(open("config.json", "r").read())
 
-base = json.loads(open("configs/base.json", "r").read())
-d = dict()
-d['clipboard'] = 0
-d['plugins'] = dict()
+def read_file(filename):
+    """Simple file read using with"""
+    with open(filename, "r", encoding="UTF-8") as _f:
+        return _f.read()
+
+
+def write_file(filename, data):
+    """Simple file write using with"""
+    with open(filename, "w", encoding="UTF-8") as _f:
+        _f.write(data)
+
+
+config = json.loads(read_file(("config.json")))
+
+base = json.loads(read_file(("configs/base.json")))
+d = {
+    "clipboard": 0,
+    "plugins": {}
+}
 base['default'] = d
-env = json.loads(open("configs/env.json", "r").read())
-yes = json.loads(open("configs/yes.json", "r").read())
-no = json.loads(open("configs/no.json", "r").read())
-dev = json.loads(open("configs/dev.json", "r").read())
+env = json.loads(read_file(("configs/env.json")))
+yes = json.loads(read_file(("configs/yes.json")))
+_no = json.loads(read_file(("configs/no.json")))
+dev = json.loads(read_file(("configs/dev.json")))
 
-plugins = json.loads(open('plugins.json', 'r').read())
+plugins = json.loads(read_file(("plugins.json")))
+
 
 def set_plugins(changes):
+    """Apply changes (like a mask) to an entry in the plugins dictionary"""
     for category, c_obj in changes.items():
-        if type(c_obj) is bool:
+        if isinstance(c_obj, bool):
             for plugin in plugins[category]:
                 plugins[category][plugin]['default'] = True
         else:
             for plugin, status in c_obj.items():
                 plugins[category][plugin]['default'] = status
 
+
 def parse_config():
+    """Read through user's config file and set plugin data accordingly"""
     # Base
-    if not type(config['base']) is str:
-        print("Error in config file. Expected string for 'base'.")
-    elif len(config['base']) == 0 or not config['base'] in base:
+    if len(config['base']) == 0 or not config['base'] in base:
         config['base'] = 'default'
     set_plugins(base[config['base']]['plugins'])
     # Envs
-    if not type(config['environment']) is list:
-        print("Error in config file. Expected [] list for 'environment'.")
-    else:
-        for environment in config['environment']:
-            if environment in env:
-                set_plugins(env[environment])
+    for environment in config['environment']:
+        if environment in env:
+            set_plugins(env[environment])
     # Yes
-    if not type(config['yes']) is list:
-        print("Error in config file. Expected [] list for 'yes'.")
-    else:
-        for pack in config['yes']:
-            if pack in yes:
-                set_plugins(yes[pack])
+    for pack in config['yes']:
+        if pack in yes:
+            set_plugins(yes[pack])
     # No
-    if not type(config['no']) is list:
-        print("Error in config file. Expected [] list for 'no'.")
-    else:
-        for pack in config['no']:
-            if pack in no:
-                set_plugins(no[pack])
+    for pack in config['no']:
+        if pack in _no:
+            set_plugins(_no[pack])
     # Dev
-    if not type(config['dev']) is list:
-        print("Error in config file. Expected [] list for 'dev'.")
-    else:
-        for pack in config['dev']:
-            if pack in dev:
-                set_plugins(dev[pack])
+    for pack in config['dev']:
+        if pack in dev:
+            set_plugins(dev[pack])
     # Colors
-    if not type(config['colors']) is list:
-        print("Error in config file. Expected [] list for 'colors'.")
-    else:
-        for color in config['colors']:
-            if color in plugins['colorschemes']:
-                plugins['colorschemes'][color]['default'] = True
+    for color in config['colors']:
+        if color in plugins['colorschemes']:
+            plugins['colorschemes'][color]['default'] = True
+
 
 def copy_vim_script(category, plug):
+    """Copy over the vim-script file for a designated plugin (if it exists)"""
     src = f"settings/{category}/{plug}.vim"
     if os.path.exists(src):
         dst = 'nvim/plug-set'
-        if category == 'coc' or category == 'nerdtree':
+        if category in {"coc", "nerdtree"}:
             if category == plug:
                 dst += f"/{category}.vim"
             else:
@@ -82,97 +88,136 @@ def copy_vim_script(category, plug):
             dst += f"/{category}_{plug}.vim"
         os.system(f"cp {src} {dst}")
 
+
+_NL = '\n'
+
+
+def construct_plugin_line(category: str, plug: str, plugin: dict):
+    """Construct a "Plug ..." line for use with Vim-Plug"""
+    line = ""
+    if category == "nerdtree":
+        if plug != "nerdtree":
+            line += " |\n\t\\ "
+        line += f'Plug \'{plugin["repo"]}\''
+        if "params" in plugin:
+            line += f', {plugin["params"]}'
+        return line, False
+    line = f'Plug \'{plugin["repo"]}\''
+    if "params" in plugin:
+        line += f', {plugin["params"]}'
+    if "comment" in plugin:
+        line += f" \" {plugin['comment']}"
+    return line, True
+
+
 def build_plugins():
+    """Construct the vim-plug.vim file (plugin loader)"""
     print("\033[1;32mSetting up Plugins...\033[0m")
-    print("\033[1;31mConstructing nvim/vim-plug.vim... and copying plugin settings to nvim/plug-set/\033[0m")
-    file = open("nvim/vim-plug.vim", "w", newline = '')
-    file.write("\"\n\" Vim-Plug\n\"\ncall plug#begin(stdpath('data') . '/plugged')")
+    print(
+        "\033[1;31mConstructing nvim/vim-plug.vim... and copying plugin "
+        "settings to nvim/plug-set/\033[0m")
+    data = """"
+" Vim-Plug
+"
+call plug#begin(stdpath('data') . '/plugged')"""
     # Go through plugins dir
-    #original_dir = os.getcwd()
-    #os.chdir(original_dir + "/plugins")
+    # original_dir = os.getcwd()
+    # os.chdir(original_dir + "/plugins")
     # Variable init
     for category in list(plugins):
-        file.write(f"\n\" {category}\n")
+        data += f"\n\" {category}\n"
         # Special category rules
-        if category == "coc" or category == "nerdtree":
+        if category in {"coc", "nerdtree"}:
             if not plugins[category][category]['default']:
                 continue
-            file.write(f'" {plugins[category][category]["comment"]}\n')
+            data += f'" {plugins[category][category]["comment"]}\n'
             for sub, obj in plugins[category].items():
                 if sub != category and obj['default']:
-                    file.write(f'" \t{plugins[category][sub]["comment"]}\n')
+                    data += f'" \t{plugins[category][sub]["comment"]}\n'
                     copy_vim_script(category, sub)
             copy_vim_script(category, category)
             if category == "coc":
                 plugin = plugins[category][category]
-                line = f'Plug \'{plugin["repo"]}\''
-                if "params" in plugin:
-                    line += f', {plugin["params"]}'
-                file.write(f"{line}\n")
+                data += f'Plug \'{plugin["repo"]}\'' + (
+                    f', {plugin["params"]}\n' if "params" in plugin else "\n")
                 # Gather settings
-                coc_settings = [f"// {name}\n" + open(f"settings/{category}/{name}.json").read() for name, obj in plugins[category].items() if obj['default'] and os.path.exists(f"settings/{category}/{name}.json")]
+                coc_settings = [
+                    f"// {name}\n"
+                    f"{read_file(f'settings/{category}/{name}.json')}"
+                    for name, obj in plugins[category].items()
+                    if obj['default'] and os.path.exists(
+                        f"settings/{category}/{name}.json")]
                 if len(coc_settings) > 0:
-                    open('nvim/coc-settings.json', 'w').write('{\n' + '\n'.join(coc_settings) + '\n"": ""\n}\n')
+                    write_file(
+                        "nvim/coc-settings.json",
+                        f'{{\n{_NL.join(coc_settings)}\n"": ""\n}}\n')
                 continue
         for plug in list(plugins[category]):
             # Read file?
             plugin = plugins[category][plug]
             if plugin['default']:
-                if category == "nerdtree":
-                    if plug == "nerdtree":
-                        line = f'Plug \'{plugin["repo"]}\''
-                        if "params" in plugin:
-                            line += f', {plugin["params"]}'
-                        file.write(line)
-                    else:
-                        line = f' |\n\t\\ Plug \'{plugin["repo"]}\''
-                        if "params" in plugin:
-                            line += f', {plugin["params"]}'
-                        file.write(line)
+                line, _nc = construct_plugin_line(category, plug, plugin)
+                if not _nc:
+                    data += line
                     continue
-                line = f'Plug \'{plugin["repo"]}\''
-                if "params" in plugin:
-                    line += f', {plugin["params"]}'
-                if "comment" in plugin:
-                    line += f" \" {plugin['comment']}"
-                file.write(f'{line}\n')
+                data += f'{line}\n'
                 copy_vim_script(category, plug)
     if plugins['nerdtree']['nerdtree']['default']:
-        file.write('\n')
-    file.write('call plug#end()\n')
+        data += '\n'
+    data += 'call plug#end()\n'
     if plugins['coc']['coc']['default']:
-        file.write('let g:coc_global_extensions = [')
-        file.write(', '.join(["'" + obj['cocinstall'] + "'" for name, obj in plugins['coc'].items() if name != 'coc' and obj['default']]))
-        file.write(']\n')
+        data += 'let g:coc_global_extensions = ['
+        data += ', '.join([
+            f"'{obj['cocinstall']}'"
+            for name, obj in plugins['coc'].items()
+            if name != 'coc' and obj['default']])
+        data += ']\n'
+    write_file("nvim/vim-plug.vim", data)
     print('\033[1;31mDone\033[0m')
-    file.close()
-    #os.chdir(original_dir)
+    # os.chdir(original_dir)
     # Install/Update
-    #os.system('main/update.bash')
+    # os.system('main/update.bash')
 
-def set_colorscheme(rc, name):
-    rc.write(f"colorscheme {name}\n")
+
+def set_colorscheme(name):
+    """Set the colorscheme for NeoVim and lightline"""
     if plugins['statusbar']['lightline']['default']:
-        with open('nvim/plug-set/statusbar_lightline.vim', 'a') as vs:
-            vs.write(f"let g:lightline.colorscheme = '{name}'")
-            vs.close()
+        with open(
+            'nvim/plug-set/statusbar_lightline.vim',
+            'a',
+            encoding="UTF-8"
+        ) as _vs:
+            _vs.write(f"let g:lightline.colorscheme = '{name}'")
+    return f"colorscheme {name}\n"
+
 
 def create_init():
-    rc = open('nvim/init.vim', 'w')
-    # TODO: make this more adaptable
-    for p in ['0_truecolor.vim', '1_cursor.vim', '2_clipboard.vim', '3_plugins.vim', '80_binary.vim', '81_spell.vim', '90_remaps.vim', '99_general.vim']:
-        with open('init/' + p, 'r') as part:
-            rc.write(part.read())
-            part.close()
+    """Construct the primary NeoVim config file (init.vim)"""
+    # TO-DO: make this more adaptable
+    data = "".join(read_file(f"init/{_p}") for _p in [
+        '0_truecolor.vim',
+        '1_cursor.vim',
+        '2_clipboard.vim',
+        '3_plugins.vim',
+        '80_binary.vim',
+        '81_spell.vim',
+        '90_remaps.vim',
+        '99_general.vim'
+    ])
 
     # Read config files
-    selected_base = base['default' if len(config['base']) == 0 or not config['base'] in base else config['base']]
+    selected_base = base[
+        'default'
+        if len(config['base']) == 0 or not config['base'] in base
+        else config['base']]
 
     # Clipboard
     print("\033[1;32mSetting up Clipboard Provider...\033[0m")
     # if config doesn't specify, then ask
-    if not "clipboard" in selected_base:
-        print("Select System Environment\n\t0) None\n\t1) KDE\n\t2) Xfce\n\t3) Termux")
+    if "clipboard" not in selected_base:
+        print(
+            "Select System Environment"
+            "\n\t0) None\n\t1) KDE\n\t2) Xfce\n\t3) Termux")
         selected_base['clipboard'] = int(input())
     if selected_base['clipboard'] == 0:
         print("Continuing without custom clipboard provider.")
@@ -181,13 +226,19 @@ def create_init():
         print("Neovim will use Klipper to Copy/Paste.")
         os.system('cp main/kde_clipboard.vim nvim/clipboard.vim')
         home = os.path.expanduser('~')
-        if os.path.exists(home + '/.local/bin') and os.path.isdir(home + '/.local/bin') and not os.path.exists(home + '/.local/bin/klipperCopy'):
+        if (
+            os.path.exists(home + '/.local/bin')
+            and os.path.isdir(home + '/.local/bin')
+            and not os.path.exists(home + '/.local/bin/klipperCopy')
+        ):
             os.system(f'cp main/klipperCopy {home}/.local/bin/')
     elif selected_base['clipboard'] == 2:
         print("Xfce4 clipboard should work with Neovim out-of-the-box.")
         os.system('rm -f nvim/clipboard.vim')
     elif selected_base['clipboard'] == 3:
-        print("Neovim will use Termux API. Make sure to install the package, and the corresponding Android package!")
+        print(
+            "Neovim will use Termux API. Make sure to install the "
+            "package, and the corresponding Android package!")
         os.system('cp main/termux_clipboard.vim nvim/clipboard.vim')
     else:
         print("Invalid environment - continuing with setup.")
@@ -197,22 +248,60 @@ def create_init():
 
     # Colorscheme
     print("\033[1;32mSetting colorscheme\033[0m")
-    schemes_installed = dict((k, v) for k, v in plugins['colorschemes'].items() if v['default'])
+    schemes_installed = dict(
+        (k, v)
+        for k, v in plugins['colorschemes'].items()
+        if v['default'])
     if len(schemes_installed) == 0:
         print("No colorschemes were installed! :(")
     elif len(schemes_installed) == 1:
-        set_colorscheme(rc, list(schemes_installed.values())[0]['colorscheme'])
+        data += set_colorscheme(
+            list(schemes_installed.values())[0]['colorscheme'])
     elif len(config['colors']) > 0:
-        set_colorscheme(rc, schemes_installed[config['colors'][0]]['colorscheme'])
+        data += set_colorscheme(
+            schemes_installed[config['colors'][0]]['colorscheme'])
     else:
-        print("More than 1 colorscheme installed, please select one to use by default:")
+        print(
+            "More than 1 colorscheme installed, please select one to use "
+            "by default:")
         for name in schemes_installed:
             print(f"\t{name} - {schemes_installed[name]['comment']}")
         selection = input()
-        if selection in schemes_installed:
-            set_colorscheme(rc, schemes_installed[selection]['colorscheme'])
+        data += (
+            set_colorscheme(schemes_installed[selection]['colorscheme'])
+            if selection in schemes_installed else "")
 
-print("\033[1;31mParsing config files\033[0m")
-parse_config()
-print("\033[1;31mConstructing nvim/init.vim\033[0m")
-create_init()
+    write_file("nvim/init.vim", data)
+
+
+def check_valid_config():
+    """Verify the user's config file is properly structured"""
+    good_config = True
+    if not isinstance(config['base'], str):
+        print("Error in config file. Expected string for 'base'.")
+        good_config = False
+    if not isinstance(config['environment'], list):
+        print("Error in config file. Expected [] list for 'environment'.")
+        good_config = False
+    if not isinstance(config['yes'], list):
+        print("Error in config file. Expected [] list for 'yes'.")
+        good_config = False
+    if not isinstance(config['no'], list):
+        print("Error in config file. Expected [] list for 'no'.")
+        good_config = False
+    if not isinstance(config['dev'], list):
+        print("Error in config file. Expected [] list for 'dev'.")
+        good_config = False
+    if not isinstance(config['colors'], list):
+        print("Error in config file. Expected [] list for 'colors'.")
+        good_config = False
+    return good_config
+
+
+if __name__ == "__main__":
+    print("\033[1;31mParsing config files\033[0m")
+    if not check_valid_config():
+        sys.exit(1)
+    parse_config()
+    print("\033[1;31mConstructing nvim/init.vim\033[0m")
+    create_init()
