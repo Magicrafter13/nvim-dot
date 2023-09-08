de107725537fff2806a0aad5fb7ccf80bc310905
Matthew Rease <scubaventure101@hotmail.com>
2023-09-08 14:01:06 -0700

    Change how makefiles are generated
    Will likely touch these more in the future

--- Makefile
+++ Makefile
@@ -1,6 +1,4 @@
 default:
-# Clean some files
-#	@$(MAKE) -e clean-partial --no-print-directory
 # Compile lua config files
 	@$(MAKE) -e all --no-print-directory
 
@@ -8,7 +6,7 @@ directories:
 	@mkdir -p nvim/lua/plug-set/coc
 	@mkdir -p nvim/lua/plug-set/nerdtree
 
-all: directories nvim/lua/plug-set/settings.make nvim/init.lua nvim/lua/clipboard.lua nvim/lua/colorscheme.lua nvim/lua/plug-set/init.lua nvim/lua/lazy-init.lua
+all: directories nvim/lua/plug-set/init.lua nvim/init.lua nvim/lua/clipboard.lua nvim/lua/colorscheme.lua nvim/lua/lazy-init.lua
 	@$(MAKE) -s -f nvim/lua/plug-set/settings.make
 	@$(MAKE) -s -f nvim/lua/plug-set/lsp.make
 # Update/cleanup plugins
@@ -23,18 +21,19 @@ clean:
 config:
 	@./init.py
 
-#clean-partial:
-#	@rm -f nvim/coc-settings.json
-
 uninstall:
 	@./main/uninstall.bash
 
-.PHONY: directories all clean uninstall #clean-partial
+.PHONY: directories all clean uninstall
 
-nvim/lua/plug-set/settings.make nvim/lua/plug-set/lsp.make: config.json .plugins
-	@main/makefiles.py
-	@$(MAKE) -s -f nvim/lua/plug-set/settings.make
+nvim/lua/plug-set/settings.make nvim/lua/plug-set/lsp.make: config.json .plugins main/makefiles.py
+	@main/makefiles.py $@
+
+nvim/lua/plug-set/nvim-lspconfig.lua: nvim/lua/plug-set/lsp.make
 	@$(MAKE) -s -f nvim/lua/plug-set/lsp.make
+
+nvim/lua/plug-set/init.lua: nvim/lua/plug-set/settings.make nvim/lua/plug-set/nvim-lspconfig.lua
+	@$(MAKE) -s -f nvim/lua/plug-set/settings.make
 
 # Add init.py as dependency once it is setup to read the config file
 config.json:
@@ -50,11 +49,10 @@ nvim/init.lua: init/0_truecolor.lua init/1_cursor.lua init/2_clipboard.lua init/
 nvim/lua/clipboard.lua: main/clipboard.py config.json configs/base.json
 	@main/clipboard.py
 
-nvim/lua/colorscheme.lua: config.json .plugins plugins.json
+nvim/lua/colorscheme.lua: .plugins plugins.json
 	@main/colorscheme.py
 
 nvim/lua/lazy-init.lua: .plugins plugins.json
-#	@rm -rf nvim/lua/plug-set/*.lua nvim/lua/plug-set/coc/*.lua nvim/lua/plug-set/nerdtree/*.lua
 	@main/lazy.py
 # Symlink directories and files to standard system locations
 	@./main/link.bash
--- main/makefiles.py
+++ main/makefiles.py
@@ -2,33 +2,31 @@
 """Construct various Makefiles based on data in .plugins."""
 
 import json
-import os
+import sys
 
 from utils import read_file, write_file  # pylint: disable=import-error
 
 plugins = json.loads(read_file(".plugins"))["plugins"]
 config = json.loads(read_file("config.json"))
 
-ENL = "\\n"
+LSP = "lsp" in config["programming"]
 LSP_REQ = (
     "\techo 'require(\"plug-set/nvim-lspconfig\")' >> nvim/lua/plug-set/"
     "init.lua")
 
-if __name__ == "__main__":
-    cwd = os.getcwd()
 
-    LSP = "lsp" in config["programming"]
-
+def create_settings():
+    """Create settings.make."""
     #
     # CREATE nvim/lua/plug-set/settings.make
     # FOR    nvim/lua/plug-set/init.lua
     #
-    FILES = " ".join([f"settings/{plugin}.lua" for plugin in plugins])
+    files = " ".join([f"settings/{plugin}.lua" for plugin in plugins])
     if LSP:
-        FILES += " settings/nvim-lspconfig.lua"
+        files += " settings/nvim-lspconfig.lua"
     write_file(
         "nvim/lua/plug-set/settings.make",
-        f"""POSSIBLE := {FILES}
+        f"""POSSIBLE := {files}
 FILES    := $(wildcard settings/*.lua)
 EXIST    := $(filter $(POSSIBLE),$(FILES))
 
@@ -43,18 +41,21 @@ else
 endif
 """)  # noqa: E501  pylint: disable=line-too-long
 
+
+def create_lsp():
+    """Create lsp.make."""
     #
     # CREATE nvim/lua/plug-set/lsp.make
     # FOR    nvim/lua/plug-set/nvim-lspconfig.lua
     #
-    FILES = " ".join([
+    files = " ".join([
         f"settings/lspconfig/{lang}.lua"
         for lang in config["dev"]])
     if LSP:
         write_file(
             "nvim/lua/plug-set/lsp.make",
             # pylint: disable=C0301
-            f"""POSSIBLE := {FILES}
+            f"""POSSIBLE := {files}
 FILES    := $(wildcard settings/lspconfig/*.lua)
 EXIST    := $(filter $(POSSIBLE),$(FILES))
 
@@ -72,3 +73,11 @@ endif
 """)  # noqa: E501
     else:
         write_file("nvim/lua/plug-set/lsp.make", "all:")
+
+
+if __name__ == "__main__":
+    match sys.argv[1]:
+        case "nvim/lua/plug-set/settings.make":
+            create_settings()
+        case "nvim/lua/plug-set/lsp.make":
+            create_lsp()
