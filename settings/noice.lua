39f6c2b0b891500943c29aca05db1ba6e068704d
Matthew Rease <scubaventure101@hotmail.com>
2023-09-11 15:10:53 -0700

    Forgot to include these files

---
+++ settings/noice.lua
@@ -0,0 +1,14 @@
+require'noice'.setup{
+	cmdline = {
+		enabled = true,
+		view = "cmdline_popup"
+	},
+	messages = {
+		enabled = true,
+		view = "notify"
+	},
+	popupmenu = {
+		enabled = true,
+		backend = "nui"
+	}
+}
