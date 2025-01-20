a050f9c98fdbb98cab8400e19cb3d94dfa2832da
Matthew Rease <self@matthewrease.net>
2025-01-19 23:24:44 -0800

    Remove old CSS stuff, add Git UI and TODO highlight.
    Replaced the CSS color highlighter with a more generic solution, and
    removed the syntax plugin since Treesitter likely far exceeds it.
    
    Making my first commit with "Neogit", it's... interesting.
    
    TODO: HACK: NOTE: and more now highlight and are searchable with
    telescope.

---
+++ settings/neogit.lua
@@ -0,0 +1,11 @@
+local neogit = require('neogit')
+
+neogit.setup {
+	git_services = {
+    ["github.com"] = "https://github.com/${owner}/${repository}/compare/${branch_name}?expand=1",
+    ["bitbucket.org"] = "https://bitbucket.org/${owner}/${repository}/pull-requests/new?source=${branch_name}&t=1",
+    ["gitlab.com"] = "https://gitlab.com/${owner}/${repository}/merge_requests/new?merge_request[source_branch]=${branch_name}",
+    ["azure.com"] = "https://dev.azure.com/${owner}/_git/${repository}/pullrequestcreate?sourceRef=${branch_name}&targetRef=${target}",
+		["gitlab.matthewrease.net"] = "https://gitlab.com/${owner}/${repository}/merge_requests/new?merge_request[source_branch]=${branch_name}",
+  },
+}
