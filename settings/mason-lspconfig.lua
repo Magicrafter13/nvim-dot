6b24264d5449df3051eb97899f68f1a0f6f2b4a7
Matthew Rease <self@matthewrease.net>
2025-01-22 11:06:16 -0800

    Mason lspconfig fix I thought I already did.

--- settings/mason-lspconfig.lua
+++ settings/mason-lspconfig.lua
@@ -1,5 +1,5 @@
 require 'mason-lspconfig'.setup {
-	automatic_installation = true
+	automatic_installation = false -- TODO: set to true once noice fixes themselves (msg_scroll_flush: Assertion `to_scroll >= 0' failed)
 }
 
 require 'mason-lspconfig'.setup_handlers {
