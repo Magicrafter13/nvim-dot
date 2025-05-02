c1cd30f26a01045b4274bf9cd4a7a75fef4b7139
Matthew Rease <self@matthewrease.net>
2025-05-02 14:15:59 -0700

    Remove Comrade plugin
    It appears to be broken right now, or not configured correctly

--- all_options.py
+++ all_options.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Generate config file with all possible choices."""
+"""Generate config file with all possible choices, except colorschemes."""
 
 import json
 
@@ -11,10 +11,11 @@ output = {
     'yes': [str(key) for key in json.loads(read_file('configs/yes.json')).keys()],
     'programming': [str(key) for key in json.loads(read_file('configs/programming.json')).keys()],
     'dev': [str(key) for key in json.loads(read_file('configs/dev.json')).keys()],
-    'colors': [
-        key
-        for key, value in json.loads(read_file('plugins.json')).items()
-        if 'attributes' in value and 'colorscheme' in value['attributes']],
+    'colors': [],
+    #'colors': [
+    #    key
+    #    for key, value in json.loads(read_file('plugins.json')).items()
+    #    if 'attributes' in value and 'colorscheme' in value['attributes']],
 }
 
 with open('config.json', 'w', encoding='UTF-8') as config:
